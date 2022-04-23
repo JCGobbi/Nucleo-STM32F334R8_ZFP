2f7c275982dcc825c819c7aac33c1c5e46ad0b0f
JCGobbi <jcgobbi@yandex.com>
2022-04-23 16:10:49 -0300

    Add new pragma to ADL/system/sys.ads.

--- ADL/system/sys.ads
+++ ADL/system/sys.ads
@@ -2,5 +2,9 @@
 
 package Sys is
    pragma Pure;
-
+   --  Note that we take advantage of the implementation permission to make
+   --  this unit Pure instead of Preelaborable; see RM 13.7.1(15). In Ada
+   --  2005, this is Pure in any case (AI-362).
+   pragma No_Elaboration_Code_All;
+   --  Allow the use of that restriction in units that WITH this unit.
 end Sys;
